1e892f02bbd338bb471a7adcefc635739ff62d2e
Danny Lin <danny@kdrag0n.dev>
2019-03-18 17:58:48 -0700

    lpm-levels: Allow enforcing deepest cluster idle state selection
    Similarly to the use_deepest_state flag added to cpuidle in commit
    d29563d2fd4f ("cpuidle: Allow enforcing deepest idle state selection"),
    add a cluster_use_deepest_state flag to lpm-levels that forces the
    cluster idle state to be the deepest possible state at the time of
    selection. Note that this is *not* always the deepest state available as
    the status of RPM will influence whether the system is able to enter the
    deepest state(s).
    
    This flag can be modified from within the kernel as well as from
    userspace through a module parameter.
    
    The deepest cluster idle state available on sdm845 uses 76% less power
    than the shallowest idle state.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: starlight5234 <ifist2834@gmail.com>

--- include/soc/qcom/lpm_levels.h
+++ include/soc/qcom/lpm_levels.h
@@ -22,4 +22,5 @@ struct system_pm_ops {
 
 uint32_t register_system_pm_ops(struct system_pm_ops *pm_ops);
 void update_ipi_history(int cpu);
+void lpm_cluster_use_deepest_state(bool enable);
 #endif
